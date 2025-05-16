3e82215776cf8ae3ab9ee490a9d641f8dde8f19c
Jay-Bose <joshua.s.bowes@outlook.com>
2025-05-16 00:03:45 -0500

    started new tutorial goal

---
+++ FirstProject/source/screenColor.c
@@ -0,0 +1,21 @@
+// screenColor.c
+
+/* 
+    My first .gba program will set the color of the entire screen.
+After getting understanding how to set the screen/background color I will try to display a sprite then
+mayba a moving sprite.
+    I struggle understanding the pallettes.
+*/
+
+#include <stdio.h>
+
+
+/* Display a color on the screen using tile mode */
+
+
+int main() {
+    
+    
+
+    return 0;
+}
