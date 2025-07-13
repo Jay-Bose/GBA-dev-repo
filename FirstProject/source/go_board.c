0f511275e697c1a23dc1b15b1dc4fee15f687772
Jay-Bose <joshua.s.bowes1020@outlook.com>
2025-07-13 15:37:34 -0500

    some more random stuff added

--- FirstProject/source/go_board.c
+++ FirstProject/source/go_board.c
@@ -6,11 +6,12 @@
 #include <stdio.h>
 #include <gba_base.h>
 #include <gba_video.h>
+#include <tonc.h>
 
 // .h files
 #include "go_board.h"
 
-Go_Game_State * go_game;
+Go_Game_State go_game;
 
 
 void init_go_game() {
@@ -19,9 +20,9 @@ void init_go_game() {
     i = j = 0;
 
     // initialize all game positions to empty
-    for(; i <= GO_BRD_9X9; ++i) {
-        for(; j <= GO_BRD_9X9; ++j) {
-            go_game->go_board[i][j]->stone_type = BLACK_STONE;
+    for(; i < GO_BRD_9X9; ++i) {
+        for(; j < GO_BRD_9X9; ++j) {
+            *(go_game.go_board[i][j]->stone_type) = EMPTY_STONE;
         }
     }
     
@@ -31,16 +32,15 @@ int main() {
     
     init_go_game();
 
-    for(u16 i = 0; i <= GO_BRD_9X9; ++i) {
-        for(u16 j = 0; j <= GO_BRD_9X9; ++j) {
-            printf("%d, ",go_game->go_board[i][j]->stone_type);
-        }
-    }
+    REG_DISPCNT= DCNT_MODE0 | DCNT_BG0;
 
+	// Init BG 0 for text on screen entries.
+	tte_init_se_default(0, BG_CBB(0)|BG_SBB(31));
 
-    while(1) {
-            
-    }
+	tte_write("#{P:72,64}");		// Goto (72, 64).
+	tte_write("Hello World!");		// Print "Hello world!"
 
-    return 0;
+	while(1);
+
+	return 0;
 }
